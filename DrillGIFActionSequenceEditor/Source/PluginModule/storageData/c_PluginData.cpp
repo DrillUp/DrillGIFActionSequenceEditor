0f80c6253b45832a68f4ea34f4862187acb94235
DrillUp123 <1355126171@qq.com>
2022-03-21 09:38:53 +0800

    更新同步了工具类。

--- DrillGIFActionSequenceEditor/Source/PluginModule/storageData/c_PluginData.cpp
+++ DrillGIFActionSequenceEditor/Source/PluginModule/storageData/c_PluginData.cpp
@@ -1,9 +1,12 @@
 #include "stdafx.h"
 #include "C_PluginData.h"
 
+#include "Source/Utils/common/TTool.h"
+
 /*
 -----==========================================================-----
 		类：		插件数据 实体类.cpp
+		版本：		v1.04
 		作者：		drill_up
 		所属模块：	插件模块
 		功能：		存储插件信息的类，对应plugin.js中每条插件的配置数据。
@@ -13,15 +16,86 @@
 -----==========================================================-----
 */
 C_PluginData::C_PluginData(){
-	this->name = "";					//存储数据 - 插件名
-	this->status = false;				//存储数据 - 状态
-	this->description = "";				//存储数据 - 插件描述
-	this->parameters = QJsonObject();	//存储数据 - 参数
+	this->name = "";
+	this->status = false;
+	this->description = "";
+	this->parameters = QJsonObject();
+
+	this->temp_inited = false;
+	this->temp_version = "";			//版本(调取时才初始化）
+	this->temp_type = "";				//类型（调取时才初始化）
+	this->temp_name = "";				//中文名（调取时才初始化）
 }
 C_PluginData::~C_PluginData(){
 }
 
 
+
+//数据 - 初始化截取段
+void C_PluginData::initDescription_data(){
+	this->temp_inited = true;
+
+	// > 版本
+	if (this->description.isEmpty() == false){			//（根据方括号截取内容）
+		QString data = this->description.trimmed();
+		QChar ch = data.at(0);
+		if (ch == '[' || ch == '(' || ch == '（' || data.at(0) == 'v' || data.at(0) == 'V'){
+			QStringList data_list = this->description.split("]");
+			if (data_list.count() >= 2){
+				this->temp_version = data_list.at(0) + "]";
+			}
+			else{
+				data_list = this->description.split(" ");
+				if (data_list.count() >= 2){
+					this->temp_version = data_list.at(0);
+				}
+			}
+		}
+	}
+
+	// > 类型
+	QStringList data_list = this->description.split(" - ");	//（取横杠的左侧内容）
+	if (data_list.count() >= 2){
+		QStringList data_list2 = data_list.at(0).trimmed().split(QRegExp("[ \\]\\)]+"));
+		this->temp_type = data_list2.last();
+		if (this->temp_type.contains(".")){ this->temp_type = ""; }		//（含特殊字符的类型去掉）
+		if (this->temp_type.contains("[")){ this->temp_type = ""; }
+		if (this->temp_type.contains("]")){ this->temp_type = ""; }
+		if (this->temp_type.contains("-")){ this->temp_type = ""; }
+	}
+
+	// > 中文名
+	QStringList data_list2 = this->description.split(" ");
+	this->temp_name = data_list2.last();
+}
+//数据 - 获取版本（插件描述 截取段）
+QString C_PluginData::getDescription_version(){
+	if (this->temp_inited == false){ this->initDescription_data(); }
+	return this->temp_version;
+}
+double C_PluginData::getDescription_versionNum(){
+	if (this->temp_inited == false){ this->initDescription_data(); }
+	if (this->temp_version.isEmpty()){ return -1; }
+	return TTool::_to_double_(this->temp_version);
+}
+//数据 - 获取类型（插件描述 截取段）
+QString C_PluginData::getDescription_type(){
+	if (this->temp_inited == false){ this->initDescription_data(); }
+	return this->temp_type;
+}
+//数据 - 获取中文名（插件描述 截取段）
+QString C_PluginData::getDescription_name(){
+	if (this->temp_inited == false){ this->initDescription_data(); }
+	return this->temp_name;
+}
+
+
+/*-------------------------------------------------
+		空判断
+*/
+bool C_PluginData::isNull(){
+	return this->name == "";
+}
 /*-------------------------------------------------
 		运算符重载
 */
--- DrillGIFActionSequenceEditor/Source/PluginModule/storageData/s_PluginDataContainer.h
+++ DrillGIFActionSequenceEditor/Source/PluginModule/storageData/s_PluginDataContainer.h
@@ -5,7 +5,7 @@
 /*
 -----==========================================================-----
 		类：		插件数据 容器.h
-		版本：		v1.03
+		版本：		v1.04
 		作者：		drill_up
 		所属模块：	插件模块
 		功能：		管理plugin.js中的全部插件配置数据。
@@ -21,26 +21,7 @@ class S_PluginDataContainer : public QObject
 		S_PluginDataContainer();
 		~S_PluginDataContainer();
 		static S_PluginDataContainer* cur_manager;			//单例
-		static S_PluginDataContainer* getInstance();		//单例，获取自己（必须要拿到全局配置才能进行计算）
-
-
-	//-----------------------------------
-	//----读取
-	public:
-										//读取 - 读取数据（plugins.js文本）
-		void loadPluginData(QString data_context);
-										//读取 - 读取数据（plugins.js文本，不发数据变化信号）
-		void loadPluginDataNoSignal(QString data_context);
-	private:
-										//读取 - 读取数据（私有）
-		QList<C_PluginData*> readPluginData(QString data_context);
-		
-	//-----------------------------------
-	//----写入
-	public:
-										//写入 - 写入数据（plugins.js文本）
-		QString writePluginData();
-
+		static S_PluginDataContainer* getInstance();			//单例，获取自己（必须要拿到全局配置才能进行计算）
 
 	//-----------------------------------
 	//----数据
@@ -48,15 +29,12 @@ class S_PluginDataContainer : public QObject
 		QList<C_PluginData*> data_PluginDataTank;
 	public:
 										//数据 - 获取全部插件数据
-										//		【说明】：获取数据前，要确保你之前 手动 执行了load读取，不然获取不到数据。
+										//		【说明】：获取数据前，要确保你之前 手动 执行了read读取，不然获取不到数据。
 		QList<C_PluginData*> getPluginDataTank();
 										//数据 - 获取对应的插件数据
-										//		【说明】：获取数据前，要确保你之前 手动 执行了load读取，不然获取不到数据。
+										//		【说明】：获取数据前，要确保你之前 手动 执行了read读取，不然获取不到数据。
 										//		【说明】：没有则返回空指针。
 		C_PluginData* getPluginData(QString pluginName);
-	signals:
-										//数据 - 数据变化（信号）
-		void pluginDataReloaded();
 
 	//-----------------------------------
 	//----操作
@@ -67,6 +45,35 @@ class S_PluginDataContainer : public QObject
 		void op_modify(C_PluginData* data);
 										//操作 - 删除
 		void op_remove(C_PluginData* data);
+
+
+	//-----------------------------------
+	//----读取
+	public:
+										//读取 - 读取数据（plugins.js文本）
+		void loadPluginData(QString data_context);
+										//读取 - 读取数据（plugins.js文本，不发信号）
+		void loadPluginDataNoSignal(QString data_context);
+										//读取 - 一次性读取数据（plugins.js文本，读取后直接返回列表，不纳入容器）
+		QList<C_PluginData*> loadPluginDataDirectly(QString data_context);
+	signals:
+										//读取 - 数据已读取（信号）
+		void pluginDataReloaded();
+	private:
+										//读取 - 读取数据（私有）
+		QList<C_PluginData*> readPluginDataPrivate(QString data_context);
+		
+
+	//-----------------------------------
+	//----写入
+	public:
+										//写入 - 写入数据（plugins.js文本）
+		QString writePluginData();
+										//写入 - 一次性写入数据（plugins.js文本，不包括容器的数据）
+		QString writePluginDataDirectly(QList<C_PluginData*> data_list);
+	private:
+										//写入 - 写入数据（私有）
+		QString writePluginDataPrivate(QList<C_PluginData*> data_list);
 
 
 };
