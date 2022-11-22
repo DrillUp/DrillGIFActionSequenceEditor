54d7605141365a789a66305714666d5f4b781796
drill_up <1355126171@qq.com>
2022-11-22 08:38:13 +0800

    乱码修复

--- DrillGIFActionSequenceEditor/Source/Utils/widgetForm/RadioTable/C_RaTConfig.h
+++ DrillGIFActionSequenceEditor/Source/Utils/widgetForm/RadioTable/C_RaTConfig.h
@@ -1,14 +1,14 @@
-#pragma once
+﻿#pragma once
 #include "stdafx.h"
 
 
 /*
 -----==========================================================-----
-		�ࣺ		�������� ������.h
-		���ߣ�		drill_up
-		����ģ�飺	����ģ��
-		���ܣ�		��ѡ�������� �������ࡣ
-					����ϸ��cpp��
+		绫伙細		琛ㄦ牸閰嶇疆 鏁版嵁绫?cpp
+		浣滆€咃細		drill_up
+		鎵€灞炴ā鍧楋細	宸ュ叿妯″潡
+		鍔熻兘锛?	鍗曢€夎〃鏍奸厤缃?鐨勬暟鎹被銆?
+					锛堣缁嗚cpp锛?
 -----==========================================================-----
 */
 class C_RaTConfig{
@@ -18,21 +18,21 @@ class C_RaTConfig{
 		~C_RaTConfig();
 		
 	//-----------------------------------
-	//----��������
+	//----鏁版嵁
 	public:
-		bool showNumber;			//��ʾ����
-		bool zeroFill;				//������
-		int zeroFillCount;			//����λ��
-		QChar zeroFillChar;			//�����ַ�
+		bool showNumber;			//鏄剧ず搴忓彿
+		bool zeroFill;				//闆跺～鍏?
+		int zeroFillCount;			//濉厖浣嶆暟
+		QChar zeroFillChar;			//濉厖瀛楃
 
-		int rowHeight;				//�и�
-		bool isMultiSelect;			//��ѡ����
+		int rowHeight;				//琛岄珮
+		bool isMultiSelect;			//澶氶€夊紑鍏?
 
 	//-----------------------------------
-	//----������
+	//----绫诲睘鎬?
 	public:
-									//ʵ���� -> QJsonObject
+									//瀹炰綋绫?-> QJsonObject
 		QJsonObject getJsonObject();
-									//QJsonObject -> ʵ����
+									//QJsonObject -> 瀹炰綋绫?
 		void setJsonObject(QJsonObject obj);
 };
